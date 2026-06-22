c67ef449221651c97f53d964e0ee8d4692c6b4dd
AndreGuilherme404 <ag33344531@gmail.com>
2026-06-22 14:24:47 -0300

    tela do gerente

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import {Router, RouterOutlet} from '@angular/router';
+import {Footer} from './footer/footer';
+import {Header} from './header/header';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  mostrarMenu : boolean = false;
+  constructor(private router: Router){
+    this.router.events.subscribe(() => {
+      if(this.router.url === '/login' || this.router.url === '/'){
+        this.mostrarMenu = false;
+      }else{
+        this.mostrarMenu = true;
+      }
+    })
+  };
   protected readonly title = signal('HemocentroFront');
+
+
 }
--- src/app/tiposUsuario/gerente/gerente.ts
+++ src/app/tiposUsuario/gerente/gerente.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-gerente',
   imports: [],
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './gerente.css',
 })
 export class Gerente {
+  constructor(private router: Router){};
 
+  gerenciarHemocentro(){
+    this.router.navigate(['/gerenciarHemocentro']);
+  }
+  gerenciarExame(){
+    this.router.navigate(['/gerenciarExame']);
+  }
+  gerenciarPessoa(){
+    this.router.navigate(['/gerenciarPessoa']);
+  }
+  gerenciarUsuario(){
+    this.router.navigate(['/gerenciarUsuario']);
+  }
+  gerenciarColeta(){
+    this.router.navigate(['/gerenciarColeta']);
+  }
 }
